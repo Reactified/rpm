49dadcdca73eaa6f21981eb85fcbc6ac024952f9
Reactified <54720381+Reactified@users.noreply.github.com>
2021-06-05 18:51:59 +0000

    Create config.lua

---
+++ packages/create-elevator/controller/config.lua
@@ -0,0 +1,37 @@
+{
+
+    clutchSide = "top",
+    gearshiftSide = "back",
+    doorControlSide = "bottom",
+
+    carLinkSide = "right",
+    callInputSide = "left",
+
+    doorOpenTime = 5,
+    doorMovementTime = 1,
+    movementTimeout = 30,
+
+    driveInverted = false,
+    doorInverted = false,
+
+    limitSwitchSides = "front",
+    limitSwitches = {
+        [15] = "top",
+        [14] = "bottom",
+    },
+
+    bottomLimitSwitchFloor = 1,
+    topLimitSwitchFloor = 3,
+
+    floors = {
+        [1] = 0,
+        [2] = 6,
+        [3] = 12,
+    },
+    floorIndex = {
+        1,
+        2,
+        3,
+    },
+
+}
