572edfc9f422d3195a8987b11e455c02fe90c577
Reactified <54720381+Reactified@users.noreply.github.com>
2021-06-16 20:21:42 +0000

    Update command.lua

--- packages/forceload-shop/server/command.lua
+++ packages/forceload-shop/server/command.lua
@@ -1,7 +1,7 @@
 -- Forceload Command Server
-rednet.open("left") -- THIS SHOULD ONLY BE A WIRED MODEM
+rednet.open("back") -- THIS SHOULD ONLY BE A WIRED MODEM
 
--- Persistently Store Loaded Chunks
+-- Persistent Data
 local forceloaded = {}
 local function saveData()
     local f = fs.open("/forceloaded","w")
@@ -20,11 +20,16 @@ end
 local function getID(chunkX,chunkY)
     return tostring(chunkX)..","..tostring(chunkY)
 end
+local function splitID(chunkID)
+    local chunkX = tonumber(string.sub(chunkID,1,string.find(chunkID,",")-1))
+    local chunkY = tonumber(string.sub(chunkID,string.find(chunkID,",")+1,#chunkID))
+    return chunkX, chunkY
+end
 
 -- Command Functions
 local function loadChunk(chunkX,chunkY)
     local chunkID = getID(chunkX,chunkY)
-    forceloaded[chunkID] = true
+    forceloaded[chunkID] = 0
     exec("forceload add "..tostring(chunkX*16).." "..tostring(chunkY*16))
     saveData()
     return true
@@ -37,27 +42,59 @@ local function unloadChunk(chunkX,chunkY)
     return true
 end
 
+-- Chunk Days
+local function addChunkDays(chunkX,chunkY,days)
+    local chunkID = getID(chunkX,chunkY)
+    if forceloaded[chunkID] then
+        forceloaded[chunkID] = forceloaded[chunkID] + days
+        saveData()
+    else
+        loadChunk(chunkX,chunkY)
+        forceloaded[chunkID] = days
+        saveData()
+    end
+end
+
 -- Routine
 term.clear()
 term.setCursorPos(1,1)
-print("CHUNKLOADER MAINFRAME")
-while true do
-    local id,cmd = rednet.receive()
-    if type(cmd) == "table" and cmd.chunkloader then
-        if cmd.command == "load" then
-            if type(cmd.chunkX) == "number" and type(cmd.chunkY) == "number" then
-                print("LOAD: ",cmd.chunkX,cmd.chunkY)
-                loadChunk(cmd.chunkX,cmd.chunkY)
-                rednet.send(id,true)
+print("CHUNKLOADER COMMAND SERVER")
+
+local function rednetRoutine()
+    while true do
+        local id,cmd = rednet.receive()
+        if type(cmd) == "table" and cmd.chunkloader then
+            if cmd.command == "add" then
+                if type(cmd.chunkX) == "number" and type(cmd.chunkY) == "number" and type(cmd.days) == "number" then
+                    print("ADD: ",cmd.chunkX,cmd.chunkY,cmd.days)
+                    addChunkDays(cmd.chunkX,cmd.chunkY,cmd.days)
+                    rednet.send(id,true)
+                end
+            elseif cmd.command == "list" then
+                rednet.send(id,forceloaded)
             end
-        elseif cmd.command == "unload" then
-            if type(cmd.chunkX) == "number" and type(cmd.chunkY) == "number" then
-                print("UNLOAD: ",cmd.chunkX,cmd.chunkY)
-                unloadChunk(cmd.chunkX,cmd.chunkY)
-                rednet.send(id,true)
-            end
-        elseif cmd.command == "list" then
-            rednet.send(id,forceloaded)
         end
     end
 end
+
+local function chunkTimer()
+    while true do
+        local day = os.epoch("utc")/86400000
+        local dayPercent = 1-(day-math.floor(day))
+        sleep(dayPercent*86400)
+
+        -- this code will run once a day at UTC midnight
+        print("CHUNK TIMER")
+        for chunkID,daysLeft in pairs(forceloaded) do
+            forceloaded[chunkID] = daysLeft - 1
+            if forceloaded[chunkID] <= 0 then
+                local chunkX,chunkY = splitID(chunkID)
+                print("EXPIRED: ",chunkX,chunkY)
+                unloadChunk(chunkX,chunkY)
+            end
+        end
+        saveData()
+    end
+end
+
+parallel.waitForAny(chunkTimer, rednetRoutine) -- start both functions
