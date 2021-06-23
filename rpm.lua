6f75956afd9496885416d690e37449c9e6312049
Reactified <54720381+Reactified@users.noreply.github.com>
2021-06-23 14:47:20 -0400

    added support for different package repositories

--- rpm.lua
+++ rpm.lua
@@ -2,19 +2,18 @@
 -- REACTIFIED PACKAGE MANAGER (RPM) --
 -- github.com/Reactified/rpm        --
 ---------------------------------------
-
-local function hash(str)
-    -- simple hash function
-    local int = 0
-    for i = 1, #str do
-        int = int + string.byte(string.sub(str,i,i))
-    end
-    return int
-end
+local officialRepo = "RPM Official"
+local officialUrl = "https://raw.githubusercontent.com/Reactified/rpm/main/"
 
 -- RPM Data
 local data = {
-    packages = {}
+    packages = {},
+    repos = {
+        {
+            officialRepo,
+            officialUrl,
+        }
+    }
 }
 local function saveData()
     local f = fs.open("/.rpm","w")
@@ -30,15 +29,47 @@ else
 end
 
 -- RPM Functions
-local distro = "https://raw.githubusercontent.com/Reactified/rpm/main/"
-local function getFile(str)
-    local h = http.get(distro..str)
+local function hash(str)
+    -- simple hash function
+    local int = 0
+    for i = 1, #str do
+        int = int + string.byte(string.sub(str,i,i))
+    end
+    return int
+end
+local function findRepo(repoName)
+    for i,v in pairs(data.repos) do
+        if v[1] == repoName then
+            return v[2],i
+        end
+    end
+    return false
+end
+local function getFile(str, packageRepo)
+    local repo = packageRepo or officialRepo
+    local repoUrl = findRepo(repo)
+
+    if not repoUrl then
+        return false
+    end
+
+    local h = http.get(repoUrl..str)
     if h then
         local hdata = h.readAll()
         h.close()
         return hdata
     else
         return false
+    end
+end
+local function updateRepos()
+    local repos = getFile("repositories")
+    if repos then
+        local unseralized = textutils.unserialise(repos)
+        if unseralized then
+            data.repos = unseralized
+            saveData()
+        end
     end
 end
 local function parseManifest(manifest)
@@ -74,7 +105,7 @@ local function parseDependencies(dependencies)
     return files
 end
 local log = function() end
-local function installPackage(package)
+local function installPackage(package,isDependency)
     if not package then
         log("failed: no package specified",true)
         return false,"no package specified"
@@ -86,27 +117,45 @@ local function installPackage(package)
     end
 
     log("searching for package...")
-    local manifest = getFile("packages/"..package.."/manifest")
-    if not manifest then
+    local repo,manifest,official = false
+    for i,v in pairs(data.repos) do
+        manifest = getFile("packages/"..package.."/manifest",v[1])
+        if manifest then
+            repo = v[1]
+            official = (repo == officialRepo)
+            break
+        end
+    end
+    if not repo then
         log("failed: package not found",true)
         return false,"package not found"
     end
-    log("located package "..package)
+    log("located package "..package.." in "..repo)
 
-    local dependencies = getFile("packages/"..package.."/dependencies")
+    if (shell and not official) and not isDependency then
+        log("warning: 3rd party repository",false)
+        write("confirm installation? [y/n] ")
+        local input = read()
+        if string.lower(string.sub(input,1,1)) ~= "y" then
+            log("installation aborted")
+            return
+        end
+    end
+
+    local dependencies = getFile("packages/"..package.."/dependencies",repo)
     if dependencies then
         log("downloading dependencies")
         dependencies = parseDependencies(dependencies)
         for id,dependency in pairs(dependencies) do
             if not data.packages[dependency] then
-                installPackage(dependency)
+                installPackage(dependency,true)
             end
         end
     end
 
     local manifest = parseManifest(manifest)
     for id,file in pairs(manifest) do
-        local fdata = getFile("packages/"..package.."/"..file[1])
+        local fdata = getFile("packages/"..package.."/"..file[1],repo)
         if fdata then
             log("+ PKG/"..file[1].." -> ~/"..file[2])
             f = fs.open(file[2],"w")
@@ -118,6 +167,7 @@ local function installPackage(package)
         end
     end
     
+    manifest.repo = repo
     data.packages[package] = manifest
     saveData()
     log(package.." installed!")
@@ -143,6 +193,8 @@ api = {
 
         log("uninstalling "..package.."...")
         local manifest = data.packages[package]
+        manifest.repo = nil
+
         for id,file in pairs(manifest) do
             fs.delete(file[2])
             log("- ~/"..file[2])
@@ -194,13 +246,16 @@ api = {
             end
 
             local oldmanifest = data.packages[package]
-            local manifest = getFile("packages/"..package.."/manifest")
+            local repo = oldmanifest.repo
+            oldmanifest.repo = nil
+
+            local manifest = getFile("packages/"..package.."/manifest",repo)
             if not manifest then
                 log("failed: could not fetch manifest",true)
                 return false,"failed to fetch manifest"
             end
 
-            local dependencies = getFile("packages/"..package.."/dependencies")
+            local dependencies = getFile("packages/"..package.."/dependencies",repo)
             if dependencies then
                 dependencies = parseDependencies(dependencies)
                 for id,dependency in pairs(dependencies) do
@@ -213,7 +268,7 @@ api = {
             local manifest = parseManifest(manifest)
             for id,file in pairs(manifest) do
                 if not fs.exists(file[2]) then
-                    local fdata = getFile("packages/"..package.."/"..file[1])
+                    local fdata = getFile("packages/"..package.."/"..file[1],repo)
                     if fdata then
                         f = fs.open(file[2],"w")
                         f.writeLine(fdata)
@@ -228,7 +283,7 @@ api = {
 
             for _,file in pairs(oldmanifest) do
                 if fs.exists(file[2]) then
-                    local fdata = getFile("packages/"..package.."/"..file[1])
+                    local fdata = getFile("packages/"..package.."/"..file[1],repo)
                     if fdata then
                         if hash(fdata) ~= file[3] then
                             f = fs.open(file[2],"w")
@@ -253,6 +308,7 @@ api = {
                 end
             end
 
+            manifest.repo = repo
             data.packages[package] = manifest
             saveData()
             log(package.." up to date!")
@@ -275,6 +331,13 @@ if shell then
     log = function(str,err)
         if err then
             printError(str)
+        elseif err == false then
+            local previousColor = term.getTextColor()
+            if term.isColor() then
+                term.setTextColor(colors.yellow)
+            end
+            print(str)
+            term.setTextColor(previousColor)
         else
             print(str)
         end
@@ -288,6 +351,7 @@ if shell then
             print("rpm uninstall <package>")
             print("rpm update [package]")
             print("rpm list")
+            print("rpm repos")
         end,
         install = function(package)
             api.install(package)
@@ -302,6 +366,17 @@ if shell then
             print("installed packages:")
             for i,v in pairs(api.list()) do
                 print("* "..v)
+            end
+        end,
+        repos = function(update)
+            if update then
+                print("updating repositories...")
+                updateRepos()
+            else
+                print("active repositories:")
+            end
+            for i,v in pairs(data.repos) do
+                print("#"..tostring(i)..": "..v[1])
             end
         end,
     }
